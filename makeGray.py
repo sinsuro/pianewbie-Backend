ec8e149ef9666159069dc8be90206eef39bb3c0a
sumin <jsm6616@gmail.com>
2022-03-27 22:51:13 +0900

    bit mask

---
+++ makeGray.py
@@ -0,0 +1,10 @@
+# 이진화 및 다크모드
+import cv2
+
+def preProcessing(img):
+    img=cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    ret,img=cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+    return img
+
+
--- scanFile.py
+++ scanFile.py
@@ -1,11 +1,23 @@
 import cv2
+import bitMask as bm
+import makeGray as gr
 import numpy as np
 
 address = "./test.jpg"
+
 img = cv2.imread(address)
+#여기 image를 img로 바꾸고 코드 바꾸면 될 듯?
+image=bm.removeNoise(img)
+
 line = []
 width = len(img[0])
 height = len(img)
+
+cv2.imshow("test",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# line 찾기
 for i in range(height-1):
     linePer = 0
     for j in range(width):
@@ -14,7 +26,8 @@ for i in range(height-1):
     if width * 8 / 10 < linePer:
         cv2.line(img, [100,i], [660,i], (0, 0, 255), 1) # 사진 width로 맞춰야함
         line.append(i)
+
 print(line)
 cv2.imshow("test",img)
-cv2.waitKey()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
