af1979e89f6e21a4e4b02573c121e90c95fda2df
pxxnxx <kmin9811@naver.com>
2022-03-27 23:31:44 +0900

    static import

--- scanFile.py
+++ scanFile.py
@@ -1,26 +1,25 @@
 import cv2
-import bitMask as bm
+from bitMask import removeNoise
 import numpy as np
 
 def findStaff(name):
-    img = cv2.imread(name)
-    img=bm.removeNoise(img)
+    img = removeNoise(cv2.imread(name))
 
     line = []
     width = len(img[0])
     height = len(img)
     start, end = width, 0
     for i in range(height-1):
-        linePer = 0
+        line_per = 0
         for j in range(width):
             if img[i][j] == 255:
-                linePer += 1
+                line_per += 1
                 if start > j:
                     start = j
                 if end < j:
                     end = j
-        if width * 80 / 100 < linePer and i-1 not in line: # 오선지 두께 및 좌우 간격 고려 추가 필요
-            cv2.line(img, [start-20,i], [start-10,i], (255, 255, 255), 1) # 위치 보여주기 위한 테스트 코드
+        if width * 80 / 100 < line_per and i-1 not in line: # 오선지 두께 및 좌우 간격 고려 추가 필요
+            cv2.line(img, [start-20, i], [start-10, i], (255, 255, 255), 1) # 위치 보여주기 위한 테스트 코드
             line.append(i)
     print(line)
     cv2.imshow("test",img)
