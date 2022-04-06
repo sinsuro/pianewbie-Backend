f78b0fe1749872df7fab7226f14e11909af229ab
squirMM <jsm6616@naver.com>
2022-04-06 16:17:15 +0900

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2
 if __name__ == '__main__':
     #findStaff('test.jpg')
 
-    # 블로그 코드
+    # 참고용
     img_0=modules.removeNoise(cv2.imread('test.jpg'))
     img_1,sta=modules.removeLine(img_0)
     img_2,sta=modules.nomalization(img_1,sta,10)
